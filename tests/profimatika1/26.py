305b9814c9ffe68a0a2e11511901e872390bf437
boneeex_desktop <glebpopovpersonal@gmail.com>
2026-03-27 16:38:12 +0300

    gsdioufghdsaiugyhadso8gu7adiufgbyadhjkvbzxiouhbvzxduohbfzxuoyvbzsodubzsdoubhasdzo8fgbhudzioubhzdoibuhzsdiophubdzsioubh

--- tests/profimatika1/26.py
+++ tests/profimatika1/26.py
@@ -1,6 +1,6 @@
 # https://education.yandex.ru/ege/inf/training/26/task/1?examTaskId=a1095eef-0af6-43fa-b4fd-859c2093e179&examTaskNumber=26&taskId=4ab7b0fc-334c-490e-afdf-d01720244cd1&categoryId=bef6892c-185c-4e3f-9023-80083c0cf68d
 from collections import defaultdict
-file = open(r"D:\Study\egeinfo\tests\profimatika1\26.txt").readlines()
+file = open(r"D:\informaticsclass\egeinfo\tests\profimatika1\26.txt").readlines()
 # 199154 95324
 districts = [int(line) for line in file[:199154]]
 snowrem = sorted([tuple(map(int, line.strip().split(" "))) for line in file[199154 + 1:]])
@@ -13,14 +13,20 @@ for i in snowrem:
     except:
         price[power] = sm
 
-print(snowrem[:110])
-price = sorted(price.items(), key=lambda item: -item[1])
-print(price[:100])
-# print("trolo")
+lowestp = 99024
+last = 1000
+for key, value in list(price.items())[::-1]:
+    if value < lowestp: last, lowestp = key, value
+    if value >= lowestp: 
+        price[key] = (last, lowestp)
 
-# mx = 0
-# total = 0
-# for ds in districts:
-#     total += price[ds]
-#     mx = max(mx, ds)
-# print(total, mx)
+mx = 0
+total = 0
+for ds in districts:
+    try:
+        total += price[ds]
+        mx = max(mx, ds)
+    except:
+        total += price[ds][1]
+        mx = max(mx, price[ds][0])
+print(total, mx)
--- tests/profimatika1/262.py
+++ tests/profimatika1/262.py
@@ -1,14 +1,48 @@
 # https://education.yandex.ru/ege/inf/task/f702f7af-87ae-4eab-b8f7-a8a16590cbcf
-file = open(r"D:\Study\egeinfo\tests\profimatika1\26 (2).txt")
+file = open(r"D:\informaticsclass\egeinfo\tests\profimatika1\26 (2).txt")
 # 2035 50000
 workers = sorted([(int(i.split(" ")[0]), i.split(" ")[1].strip()) for i in file], key=lambda x: [x[0], -ord(x[1])])
+otpechatok = workers.copy()
 n = 50_000
 cnt = 0
 last = -1
+accepted = []
 while n > workers[0][0]:
     last = workers.pop(0)
     n -= last[0]
     if last[1] == "G":
         cnt += 1
-print(cnt - 1, n)    
-print(len(workers))
+    accepted.append(last)
+for delta in range(10):
+    for idx, jobless in enumerate(workers):
+        if jobless[1] != "G": continue
+        if n < delta: break
+        for i, curr in enumerate(accepted):
+            if jobless[0] - delta == curr[0]:
+                n -= delta
+                cnt += 1
+                accepted[i] = jobless
+                del workers[idx]
+    print(cnt, n)
+
+
+# potential = []
+# for i in range(len(accepted)):
+#     if accepted[i][1] == "D":
+#         for idx, worker in enumerate(otpechatok):
+#             if worker[1] == "G" and worker[0] > accepted[i][0]:
+#                 potential.append((accepted[i][0], worker[0]))
+#                 del otpechatok[idx]
+#                 break
+    
+# potential = sorted(potential, key=lambda x: abs(x[0] - x[1]))
+# print(cnt, n)
+# print(potential)
+# for i in potential:
+#     delta = abs(i[0] - i[1])
+#     print(n, cnt)
+#     if n - delta >= 0:
+#         n -= delta
+#         cnt += 1
+#     print(n, cnt)
+# print(cnt, n)
